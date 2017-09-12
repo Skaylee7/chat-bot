7ba08403bec4564df81d6455eaef920584f3d7ff
SamuelDub <dubnersamuel@yahoo.com>
2017-09-12 16:31:14 -0700

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
     ping: function (message, bot) {
 
-        message.reply("Pong!\nThe bot has a ping of: `" + bot.ping + "ms`"); 
+        message.reply("Pong!\nI have a ping of: `" + bot.ping + "ms`" +
+                      "\nNot that clones can have ping..."); 
         return;
 
     }
