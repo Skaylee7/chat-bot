5440cae1fb397699aa8e09715a6bc8e6eb6321c5
SamuelDub <dubnersamuel@yahoo.com>
2017-09-12 16:28:43 -0700

    changed name

--- commands/help.js
+++ commands/help.js
@@ -21,7 +21,7 @@ module.exports = {
             .addField('?ping', "Gives you the ping of the bot", false)
             .addField('?color [color]', "Replace [color] with a valid color from #color-request", false)
             .addField('?si or ?serverinfo', "Gives you basic information on the server", false);
-            embed.addField('Mention Mom Bot', "you can talk to her!", false);
+            embed.addField('Mention Camilla\'s Clone', "you can talk to her!", false);
 
         message.author.send({
             embed
